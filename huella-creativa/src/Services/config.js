3b7c9cc52762a2f2616ecbc623ed2c7a5632b669
mbCeli <munbaporicelina@outlook.com>
2025-04-09 14:36:08 +0100

    baseURL updated

--- huella-creativa/src/Services/config.js
+++ huella-creativa/src/Services/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-    baseURL: "http://localhost:3000/api/"
+  baseURL:"https://huellacreativa-database.onrender.com",
 });
 
 export default api;
