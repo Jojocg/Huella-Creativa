8ff1ae54f611bd0fc6630a7a9cce9fc81ffaa6e3
Celina Mun Bapori <munbaporicelina@outlook.com>
2025-04-09 15:14:39 +0100

    Update config.js adding /api/ to the URL

--- huella-creativa/src/Services/config.js
+++ huella-creativa/src/Services/config.js
@@ -1,7 +1,7 @@
 import axios from 'axios'
 
 const api = axios.create({
-  baseURL:"https://huellacreativa-database.onrender.com",
+  baseURL:"https://huellacreativa-database.onrender.com/api/",
 });
 
 export default api;
